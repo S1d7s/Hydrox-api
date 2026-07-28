2566d0f1929c5a5605f66cc64e45316640d45b8c
S1d7s <welixaia8@gmail.com>
2026-07-28 19:20:50 -0300

    Update Hydrox-api.java

--- Hydrox-api.java
+++ Hydrox-api.java
@@ -1,1 +1,13 @@
 
+// Instale primeiro: npm install dotenv
+require('dotenv').config();
+
+const apiKey = process.env.API_KEY;
+console.log('Chave carregada:', apiKey);
+
+// Exemplo de uso
+fetch('https://api.exemplo.com/dados', {
+  headers: {
+    'Authorization': `Bearer ${apiKey}`
+  }
+});
